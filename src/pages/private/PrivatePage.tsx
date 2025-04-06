e4ba04353711c8e60dbc58656cd8b5375e0c691e
zach.lewis_clarity <zlewis@clarityinnovates.com>
2025-04-06 09:58:10 -0500

    groups

--- src/pages/private/PrivatePage.tsx
+++ src/pages/private/PrivatePage.tsx
@@ -6,6 +6,7 @@ const ProtectedPage: React.FC = () => {
   // Example: Displaying user information from the token
   const username = keycloak.tokenParsed?.preferred_username;
   const email = keycloak.tokenParsed?.email;
+  const roles = JSON.stringify(keycloak.tokenParsed?.realm_access?.roles)
 
   return (
     <div>
@@ -13,6 +14,7 @@ const ProtectedPage: React.FC = () => {
       <p>If you see this, you are logged in!</p>
       {username && <p>Username: {username}</p>}
       {email && <p>Email: {email}</p>}
+      {roles && <p>Roles: {roles}</p>}
       {/* You can add more logic here that requires authentication */}
       <pre style={{ backgroundColor: '#f0f0f0', padding: '10px', overflowX: 'auto' }}>
         Token Parsed: {JSON.stringify(keycloak.tokenParsed, null, 2)}
